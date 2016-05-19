f3140c573bb009fdd410aa7abceae3b23acd03e9
ihordiachenko <ihor.diachenko@student.csn.khai.edy>
2016-05-19 16:01:38 +0300

    Added DB migration: created account_statistics table.

--- src/bitbucket.org/atticlab/horizon/db2/schema/migrations/3_aggregate_expenses_for_accounts.sql
+++ src/bitbucket.org/atticlab/horizon/db2/schema/migrations/3_aggregate_expenses_for_accounts.sql
@@ -1,23 +1,23 @@
 -- +migrate Up
 
-ALTER TABLE history_accounts ADD daily_income 		decimal NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD daily_outcome 		decimal NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD weekly_income 		decimal	NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD weekly_outcome		decimal NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD monthly_income		decimal NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD monthly_outcome 	decimal NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD annual_income		decimal NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD annual_outcome		decimal NOT NULL DEFAULT 0;
-ALTER TABLE history_accounts ADD updated_at 		timestamp without time zone;
+CREATE TABLE account_statistics (
+    address             character varying(64) NOT NULL,
+    asset_code          character varying(12) NOT NULL,   
+    daily_income 		decimal NOT NULL DEFAULT 0,
+    daily_outcome 		decimal NOT NULL DEFAULT 0,
+    weekly_income 		decimal	NOT NULL DEFAULT 0,
+    weekly_outcome		decimal NOT NULL DEFAULT 0,
+    monthly_income		decimal NOT NULL DEFAULT 0,
+    monthly_outcome 	decimal NOT NULL DEFAULT 0,
+    annual_income		decimal NOT NULL DEFAULT 0,
+    annual_outcome		decimal NOT NULL DEFAULT 0,
+    updated_at 		    timestamp without time zone NOT NULL,
+    PRIMARY KEY(address, asset_code)
+);
+
+CREATE INDEX account_statistics_address_idx ON account_statistics (address);
 
 -- +migrate Down
 
-ALTER TABLE history_accounts DROP	daily_income;
-ALTER TABLE history_accounts DROP 	daily_outcome;
-ALTER TABLE history_accounts DROP 	weekly_income;
-ALTER TABLE history_accounts DROP 	weekly_outcome;
-ALTER TABLE history_accounts DROP 	monthly_income;
-ALTER TABLE history_accounts DROP 	monthly_outcome;
-ALTER TABLE history_accounts DROP 	annual_income;
-ALTER TABLE history_accounts DROP 	annual_outcome;
-ALTER TABLE history_accounts DROP   updated_at;
+DROP INDEX account_statistics_address_idx;
+DROP TABLE account_statistics;
--- src/bitbucket.org/atticlab/horizon/db2/schema/migrations/4_default_timestamp_for_accounts.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-
-ALTER TABLE history_accounts ALTER COLUMN updated_at SET DEFAULT (now() at time zone 'utc');
-
--- +migrate Down
-
-ALTER TABLE history_accounts ALTER COLUMN updated_at DROP DEFAULT;
