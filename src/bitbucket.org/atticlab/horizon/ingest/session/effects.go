5fe4dbb4ba93b110f4640abe0e68ef0668551572
Haidashenko <d.s.gaydashenko@gmail.com>
2016-08-20 13:56:13 +0300

    Removed effects

--- src/bitbucket.org/atticlab/horizon/ingest/session/effects.go
+++ src/bitbucket.org/atticlab/horizon/ingest/session/effects.go
@@ -2,15 +2,11 @@ package session
 
 import (
 	"bitbucket.org/atticlab/go-smart-base/amount"
-	"bitbucket.org/atticlab/go-smart-base/keypair"
-	"bitbucket.org/atticlab/go-smart-base/meta"
 	"bitbucket.org/atticlab/go-smart-base/xdr"
 	"bitbucket.org/atticlab/horizon/cache"
 	"bitbucket.org/atticlab/horizon/db2/history"
 	"bitbucket.org/atticlab/horizon/ingest/session/helpers"
 	"bitbucket.org/atticlab/horizon/ingest/session/ingestion"
-	"encoding/base64"
-	"fmt"
 )
 
 // EffectIngestion is a helper struct to smooth the ingestion of effects.  this
@@ -66,28 +62,6 @@ func (effects *EffectIngestion) Ingest(cursor *Cursor) {
 	opbody := cursor.Operation().Body
 
 	switch cursor.OperationType() {
-	case xdr.OperationTypeCreateAccount:
-		op := opbody.MustCreateAccountOp()
-
-		effects.Add(op.Destination, history.EffectAccountCreated,
-			map[string]interface{}{
-				"account_type": uint32(op.AccountType),
-			},
-		)
-
-		effects.Add(op.Destination, history.EffectSignerCreated,
-			map[string]interface{}{
-				"public_key": op.Destination.Address(),
-				"weight":     keypair.DefaultSignerWeight,
-			},
-		)
-
-	case xdr.OperationTypePayment:
-		op := opbody.MustPaymentOp()
-		dets := map[string]interface{}{"amount": amount.String(op.Amount)}
-		helpers.AssetDetails(dets, op.Asset, "")
-		effects.Add(op.Destination, history.EffectAccountCredited, dets)
-		effects.Add(source, history.EffectAccountDebited, dets)
 	case xdr.OperationTypePathPayment:
 		op := opbody.MustPathPaymentOp()
 		dets := map[string]interface{}{"amount": amount.String(op.DestAmount)}
@@ -115,152 +89,7 @@ func (effects *EffectIngestion) Ingest(cursor *Cursor) {
 		}
 
 		effects.ingestTrades(source, claims)
-	case xdr.OperationTypeSetOptions:
-		op := opbody.MustSetOptionsOp()
-
-		if op.HomeDomain != nil {
-			effects.Add(source, history.EffectAccountHomeDomainUpdated,
-				map[string]interface{}{
-					"home_domain": string(*op.HomeDomain),
-				},
-			)
-		}
-
-		thresholdDetails := map[string]interface{}{}
-
-		if op.LowThreshold != nil {
-			thresholdDetails["low_threshold"] = *op.LowThreshold
-		}
-
-		if op.MedThreshold != nil {
-			thresholdDetails["med_threshold"] = *op.MedThreshold
-		}
-
-		if op.HighThreshold != nil {
-			thresholdDetails["high_threshold"] = *op.HighThreshold
-		}
-
-		if len(thresholdDetails) > 0 {
-			effects.Add(source, history.EffectAccountThresholdsUpdated, thresholdDetails)
-		}
-
-		flagDetails := map[string]bool{}
-		helpers.FlagDetails(flagDetails, op.SetFlags, true)
-		helpers.FlagDetails(flagDetails, op.ClearFlags, false)
-
-		if len(flagDetails) > 0 {
-			effects.Add(source, history.EffectAccountFlagsUpdated, flagDetails)
-		}
-
-		effects.ingestSignerEffects(cursor, op)
-
-	case xdr.OperationTypeChangeTrust:
-		op := opbody.MustChangeTrustOp()
-		dets := map[string]interface{}{"limit": amount.String(op.Limit)}
-		key := xdr.LedgerKey{}
-		effect := history.EffectType(0)
-
-		helpers.AssetDetails(dets, op.Line, "")
-
-		key.SetTrustline(source, op.Line)
-
-		before, after, err := cursor.BeforeAndAfter(key)
-
-		// NOTE:  when an account trusts itself, the transaction is successful but
-		// no ledger entries are actually modified, leading to an "empty meta"
-		// situation.  We simply continue on to the next operation in that scenario.
-		if err == meta.ErrMetaNotFound {
-			return
-		}
-
-		if err != nil {
-			effects.err = err
-			return
-		}
-
-		switch {
-		case before == nil && after != nil:
-			effect = history.EffectTrustlineCreated
-		case before != nil && after == nil:
-			effect = history.EffectTrustlineRemoved
-		case before != nil && after != nil:
-			effect = history.EffectTrustlineUpdated
-		default:
-			panic("Invalid before-and-after state")
-		}
-
-		effects.Add(source, effect, dets)
-	case xdr.OperationTypeAllowTrust:
-		op := opbody.MustAllowTrustOp()
-		asset := op.Asset.ToAsset(source)
-		dets := map[string]interface{}{
-			"trustor": op.Trustor.Address(),
-		}
-		helpers.AssetDetails(dets, asset, "")
-
-		if op.Authorize {
-			effects.Add(source, history.EffectTrustlineAuthorized, dets)
-		} else {
-			effects.Add(source, history.EffectTrustlineDeauthorized, dets)
-		}
-
-	case xdr.OperationTypeAccountMerge:
-		dest := opbody.MustDestination()
-		result := cursor.OperationResult().MustAccountMergeResult()
-		dets := map[string]interface{}{
-			"amount":     amount.String(result.MustSourceAccountBalance()),
-			"asset_type": "native",
-		}
-		effects.Add(source, history.EffectAccountDebited, dets)
-		effects.Add(dest, history.EffectAccountCredited, dets)
-		effects.Add(source, history.EffectAccountRemoved, map[string]interface{}{})
-	case xdr.OperationTypeInflation:
-		payouts := cursor.OperationResult().MustInflationResult().MustPayouts()
-		for _, payout := range payouts {
-			effects.Add(payout.Destination, history.EffectAccountCredited,
-				map[string]interface{}{
-					"amount":     amount.String(payout.Amount),
-					"asset_type": "native",
-				},
-			)
-		}
-	case xdr.OperationTypeManageData:
-		op := opbody.MustManageDataOp()
-		dets := map[string]interface{}{"name": op.DataName}
-		key := xdr.LedgerKey{}
-		effect := history.EffectType(0)
-
-		key.SetData(source, string(op.DataName))
-
-		before, after, err := cursor.BeforeAndAfter(key)
-		if err != nil {
-			effects.err = err
-			return
-		}
-
-		if after != nil {
-			raw := after.Data.MustData().DataValue
-			dets["value"] = base64.StdEncoding.EncodeToString(raw)
-		}
-
-		switch {
-		case before == nil && after != nil:
-			effect = history.EffectDataCreated
-		case before != nil && after == nil:
-			effect = history.EffectDataRemoved
-		case before != nil && after != nil:
-			effect = history.EffectDataUpdated
-		default:
-			panic("Invalid before-and-after state")
-		}
-
-		effects.Add(source, effect, dets)
-	case xdr.OperationTypeAdministrative:
-		opbody.MustAdminOp()
-	// no need to duplicate data
-
 	default:
-		effects.err = fmt.Errorf("Unknown operation type: %s", cursor.OperationType())
 		return
 	}
 }
