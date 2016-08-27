213abe5f584bda6415ecce735b3e97089f09955a
Haidashenko <d.s.gaydashenko@gmail.com>
2016-08-27 11:47:06 +0300

    Redis retry to create connection

--- src/bitbucket.org/atticlab/horizon/redis/connection_provider.go
+++ src/bitbucket.org/atticlab/horizon/redis/connection_provider.go
@@ -2,12 +2,13 @@ package redis
 
 import "bitbucket.org/atticlab/horizon/log"
 
+const max_connection_reties = 10
+
 type ConnectionProviderInterface interface {
 	GetConnection() ConnectionInterface
 }
 
 type ConnectionProvider struct {
-
 }
 
 func NewConnectionProvider() ConnectionProviderInterface {
@@ -17,6 +18,14 @@ func NewConnectionProvider() ConnectionProviderInterface {
 func (c ConnectionProvider) GetConnection() ConnectionInterface {
 	if redisPool == nil {
 		log.Panic("Redis must be initialized")
+	}
+	for i := 0; i < max_connection_reties; i++ {
+		conn := redisPool.Get()
+		if conn.Err() != nil {
+			conn.Close()
+			continue
+		}
+		return NewConnection(conn)
 	}
 	return NewConnection(redisPool.Get())
 }
